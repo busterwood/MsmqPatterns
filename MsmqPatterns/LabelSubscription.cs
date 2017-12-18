f8e98b4779e8936d83f2a47275b47ef5d1f6dd48
BusterWood <chris.j.austin@gmail.com>
2017-12-18 14:03:15 +0000

    Add label subscription and callback class, intended or non-transaction queue message handling, i.e. multicast

---
+++ MsmqPatterns/LabelSubscription.cs
@@ -0,0 +1,149 @@
+﻿using BusterWood.Msmq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Diagnostics.Contracts;
+
+namespace BusterWood.MsmqPatterns
+{
+    public class LabelSubscription 
+    {
+        static readonly char[] labelSeparator = { '.' };
+        readonly Node _root = new Node("");
+
+        public IDisposable Subscribe(string label, Action<Message> callback)
+        {
+            Contract.Requires(!string.IsNullOrEmpty(label));
+            Contract.Requires(callback != null);
+
+            var star = label.IndexOf('*');
+            if (star >= 0 && star != label.Length-1)
+                throw new ArgumentException("wildcard subscriptions are only supported as the last character");
+
+            var parts = label.Split(labelSeparator);
+            lock (_root)
+            {
+                var node = _root;
+                for (int i = 0; i < parts.Length; i++)
+                {
+                    var part = parts[i];
+                    if (part.Length == 0)
+                        throw new ArgumentException("label contains an empty part");
+
+                    Contract.Assume(node != null);
+                    if (node.ChildNodes == null)
+                        node.ChildNodes = new Dictionary<string, Node>();
+
+                    Node child;
+                    if (!node.ChildNodes.TryGetValue(part, out child))
+                        node.ChildNodes.Add(part, child = new Node(part));
+                    node = child;
+                }
+
+                node.Subscriptions += callback;
+                return new Unsubscribe(node, callback, _root);
+            }
+        }
+
+        public Action<Message> Subscribers(string label)
+        {
+            Contract.Requires(!string.IsNullOrEmpty(label));
+
+            var parts = label.Split(labelSeparator);
+            lock (_root)
+            {
+
+                var node = _root;
+                foreach (var part in parts)
+                {
+                    if (part.Length == 0)
+                        throw new ArgumentException("label contains an empty part");
+                    Node child;
+                    if (node.ChildNodes == null || !node.ChildNodes.TryGetValue(part, out child))
+                        return null;
+                    Contract.Assume(child != null);
+                    node = child;
+                }
+                return node.Subscriptions;
+            }
+        }
+
+        public void Dispatch(Message msg)
+        {
+            Contract.Requires(msg != null);
+
+            Action<Message> subscriptions = null;
+            var parts = msg.Label.Split(labelSeparator);
+            lock (_root)
+            {
+                var node = _root;
+                for (int i = 0; i < parts.Length-1; i++)
+                {
+                    Node child;
+                    if (node.ChildNodes == null || !node.ChildNodes.TryGetValue(parts[i], out child))
+                        return; // no subscriptions
+                    node = child;
+                }
+
+                var lastPart = parts[parts.Length-1];
+                Node lastChild;
+                if (node.ChildNodes.TryGetValue(lastPart, out lastChild))
+                {
+                    subscriptions += lastChild.Subscriptions;
+                }
+                if (node.ChildNodes.TryGetValue("*", out lastChild)) // wild card subscription
+                {
+                    subscriptions += lastChild.Subscriptions;
+                }
+            }
+
+            if (subscriptions != null)
+            {
+                foreach (Action<Message> callback in subscriptions.GetInvocationList())
+                {
+                    try
+                    {
+                        callback(msg);
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.Error.WriteLine("WARN: " + ex);
+                    }
+                }
+            }
+        }
+
+        class Node 
+        {
+            public readonly string Name;
+            public Dictionary<string, Node> ChildNodes;
+            public Action<Message> Subscriptions;
+
+            public Node(string name)
+            {
+                Name = name;
+            }
+        }
+
+
+        class Unsubscribe : IDisposable
+        {
+            readonly Action<Message> callback;
+            readonly Node node;
+            readonly Node root;
+
+            public Unsubscribe(Node node, Action<Message> callback, Node root)
+            {
+                this.node = node;
+                this.callback = callback;
+                this.root = root;
+            }
+
+            public void Dispose()
+            {
+                lock(root)
+                    node.Subscriptions -= callback;
+            }
+        }
+    }
+}
---
+++ UnitTests/LabelSubscriptionTests.cs
@@ -0,0 +1,86 @@
+﻿using BusterWood.Msmq;
+using BusterWood.MsmqPatterns;
+using NUnit.Framework;
+using System;
+
+namespace UnitTests
+{
+    [TestFixture]
+    public class LabelSubscriptionTests
+    {
+        [TestCase("fred")]
+        [TestCase("hello.world")]
+        public void can_subscribe(string label)
+        {
+            var sub = new LabelSubscription();
+            Action<Message> callback = msg => { };
+            sub.Subscribe(label, callback);
+            var actual = sub.Subscribers(label);
+            Assert.AreEqual(callback, actual);
+        }
+
+        [Test]
+        public void can_subscribe_multiple_times()
+        {
+            var sub = new LabelSubscription();
+            Action<Message> callback1 = msg => { };
+            Action<Message> callback2 = msg => { };
+            sub.Subscribe("fred", callback1);
+            sub.Subscribe("fred", callback2);
+            var actual = sub.Subscribers("fred");
+            var subscriptions = actual.GetInvocationList();
+            Assert.IsTrue(Array.IndexOf(subscriptions, callback1)  >= 0);
+            Assert.IsTrue(Array.IndexOf(subscriptions, callback2)  >= 0);
+        }
+
+        [Test]
+        public void can_unsubscribe()
+        {
+            var sub = new LabelSubscription();
+            Action<Message> callback = msg => { };
+            var unsub = sub.Subscribe("fred", callback);
+            unsub.Dispose();
+            var actual = sub.Subscribers("fred");
+            Assert.IsNull(actual);
+        }
+
+        [TestCase("hello.world", "hello.world")]
+        [TestCase("hello.*", "hello.world")]
+        [TestCase("*", "hello")]
+        public void can_dispatch(string subscriptionLabel, string msgLabel)
+        {
+            var sub = new LabelSubscription();
+            Message actual = null;
+            Action<Message> callback = msg => actual = msg;
+            sub.Subscribe(subscriptionLabel, callback);
+            var expected = new Message { Label = msgLabel };
+            sub.Dispatch(expected);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void an_error_in_the_callback_does_not_propergate_to_the_caller()
+        {
+            var sub = new LabelSubscription();
+            Action<Message> callback = msg => { throw new InvalidOperationException(); };
+            sub.Subscribe("hello", callback);
+            var expected = new Message { Label = "hello" };
+            sub.Dispatch(expected);
+            Assert.Pass();
+        }
+
+        [Test]
+        public void an_error_in_a_callback_does_not_prevent_other_callbacks_from_being_called()
+        {
+            var sub = new LabelSubscription();
+            bool got = false;
+            Action<Message> callback1 = msg => { throw new InvalidOperationException(); };
+            Action<Message> callback2 = msg => { got = true; };
+            sub.Subscribe("hello", callback1);
+            sub.Subscribe("hello", callback2);
+            var expected = new Message { Label = "hello" };
+            sub.Dispatch(expected);
+            Assert.AreEqual(true, got);
+        }
+    }
+}
