06edc351ffc5f196ce9cb28b74aba1d89e979a53
busterwood <chris.j.austin@gmail.com>
2017-12-19 16:49:37 +0000

    Create files for nuget package

--- Busterwood.Msmq/Properties/AssemblyInfo.cs
+++ Busterwood.Msmq/Properties/AssemblyInfo.cs
@@ -6,11 +6,10 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("BusterWood.Msmq")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Easy to use messaging patterns for MSMQ")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
 [assembly: AssemblyProduct("BusterWood.Msmq")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2017")]
+[assembly: AssemblyCopyright("Copyright © BusterWood 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
