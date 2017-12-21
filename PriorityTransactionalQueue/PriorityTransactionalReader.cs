1a0c6d03edcd847c7c44db4f22b1111536b0be15
BusterWood <chris.j.austin@gmail.com>
2017-12-21 10:24:46 +0000

    Add example showing priority transsactional queue

---
+++ PriorityTransactionalQueue/PriorityTransactionalReader.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Threading.Tasks;
+namespace BusterWood.Msmq.Examples
+{
+    /// <summary>
+    /// Reader for a transactional queue whose messages have been routed into "high" and "low" subqueues by <see cref="PriorityTransactionalRouter"/>
+    /// </summary>
+    public class PriorityTransactionalReader : IDisposable
+    {
+        readonly SubQueue _low;
+        readonly SubQueue _high;
+
+        public string InputQueueFormatName { get; }
+
+        public PriorityTransactionalReader(string inputQueueFormatName)
+        {
+            Contract.Requires(inputQueueFormatName != null);
+            InputQueueFormatName = inputQueueFormatName;
+            _low = new SubQueue(InputQueueFormatName + ";low");
+            _high = new SubQueue(InputQueueFormatName + ";high");
+        }
+
+        /// <summary>Read without waiting, returns null if no message is available in high or low priority subqueues</summary>
+        public Message Read(Properties properties, QueueTransaction transaction)
+        {
+            Contract.Requires(transaction != null);
+            return _high.Read(properties, TimeSpan.Zero, transaction) ?? _low.Read(properties, TimeSpan.Zero, transaction);
+        }
+
+        public async Task<Message> PeekAsync(Properties properties, QueueTransaction transaction)
+        {
+            Contract.Requires(transaction != null);
+            var ht = _high.PeekAsync(properties);
+            if (ht.IsCompleted || ht.IsFaulted) // always give high priority queue the first chance, it may complete synchronously
+                return await ht;
+            var lt = _low.PeekAsync(properties);
+            Message[] results = await Task.WhenAll(ht, lt);
+            return results.FirstOrDefault(m => m != null);
+        }
+
+        public Message Lookup(Properties properties, long lookupId, QueueTransaction transaction, LookupAction action = LookupAction.ReceiveCurrent, TimeSpan? timeout = null)
+        {
+            Contract.Requires(transaction != null);
+            return _high.Lookup(properties, lookupId, action, timeout, transaction) ?? _low.Lookup(properties, lookupId, action, timeout, transaction);
+        }
+
+        public void Dispose()
+        {
+            _high.Dispose();
+            _low.Dispose();
+        }
+    }
+}
---
+++ PriorityTransactionalQueue/PriorityTransactionalRouter.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Diagnostics.Contracts;
+using System.Threading.Tasks;
+using BusterWood.Msmq.Patterns;
+
+namespace BusterWood.Msmq.Examples
+{
+    /// <summary>
+    /// Sample router to that moves messages to "low" or "high" subqueue, depending on the value of <see cref="Message.AppSpecific"/>
+    /// </summary>
+    public class PriorityTransactionalRouter : IProcessor
+    {
+        SubQueueRouter _router;
+        SubQueue _low;
+        SubQueue _high;
+
+        public string InputQueueFormatName { get; }
+
+        public PriorityTransactionalRouter(string inputQueueFormatName)
+        {
+            Contract.Requires(inputQueueFormatName != null);
+            if (Queue.IsTransactional(inputQueueFormatName) == QueueTransactional.None)
+                throw new ArgumentException(inputQueueFormatName + " is not a transactional queue");
+
+            InputQueueFormatName = inputQueueFormatName;
+            _router = new SubQueueRouter(inputQueueFormatName, RouterByPriority);
+        }
+
+        /// <summary>move the messages into "high" or "low" subqueues based on the value of <see cref="Message.AppSpecific"/></summary>
+        private SubQueue RouterByPriority(Message arg)
+        {
+            return arg.AppSpecific == 0 ? _low : _high;
+        }
+
+        public void Dispose()
+        {
+            _router.Dispose();
+            _low?.Dispose();
+            _high?.Dispose();
+        }
+
+        public Task<Task> StartAsync()
+        {
+            _low = new SubQueue(InputQueueFormatName + ";low");
+            _high = new SubQueue(InputQueueFormatName + ";high");
+            return _router.StartAsync();
+        }
+
+        public Task StopAsync() => _router.StopAsync();
+    }
+}
