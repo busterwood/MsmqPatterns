7996a1a1b8700cf31ae535d3db973460cea849ae
BusterWood <chris.j.austin@gmail.com>
2018-01-03 13:24:31 +0000

    Add LimitedTransaction to limit the number of open transactions

--- Busterwood.Msmq/Transaction.cs
+++ Busterwood.Msmq/Transaction.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.ComponentModel;
+using System.Threading;
 
 namespace BusterWood.Msmq
 {
@@ -19,8 +20,8 @@ namespace BusterWood.Msmq
         public static readonly QueueTransaction Single = new SpecialTransaction(3);
         
         internal readonly ITransaction InternalTransaction;
-        bool _complete;
-        bool _disposed;
+        internal bool _complete;
+        internal bool _disposed;
 
         /// <summary>Starts a new MSMQ internal transaction</summary>
         public QueueTransaction()
@@ -29,7 +30,6 @@ namespace BusterWood.Msmq
             if (res != 0)
                 throw new Win32Exception(res); //TODO: some other type of exception?
         }
-
 
         /// <summary>Commit the transaction</summary>
         public virtual void Commit()
@@ -81,6 +81,60 @@ namespace BusterWood.Msmq
 
             public override void Dispose()
             {
+            }
+        }
+    }
+
+    /// <summary>Limit the number of open transactions to avoid swamping MSMQ and bringing the host to it's knees</summary>
+    public class LimitedTransaction : QueueTransaction
+    {
+        /// <summary>The maximum number of open transactions that are allowed</summary>
+        public static int MaxOpenTransactions { get; set; } = 5000;
+
+        static readonly object _gate = new object();
+        static int _open;
+
+        /// <summary>Opens a transaction, possibly waiting if the maximum number of open transactions has been reached</summary>
+        public LimitedTransaction()
+        {
+            lock (_gate)
+            {
+                while (_open >= MaxOpenTransactions)
+                {
+                    Monitor.Wait(_gate);
+                }
+                _open++;
+            }
+        }
+
+        /// <summary>Rollback the transaction</summary>
+        public override void Abort()
+        {
+            base.Abort();
+            ReduceOpenCount();
+        }
+
+        /// <summary>Commit the transaction</summary>
+        public override void Commit()
+        {
+            base.Commit();
+            ReduceOpenCount();
+        }
+
+        /// <summary>Aborts the transaction if <see cref="M:BusterWood.Msmq.QueueTransaction.Commit" /> or <see cref="M:BusterWood.Msmq.QueueTransaction.Abort" /> has not already been called</summary>
+        public override void Dispose()
+        {
+            if (_disposed || _complete) return;
+            base.Dispose();
+            ReduceOpenCount();
+        }
+
+        static void ReduceOpenCount()
+        {
+            lock (_gate)
+            {
+                _open--;
+                Monitor.Pulse(_gate);
             }
         }
     }
